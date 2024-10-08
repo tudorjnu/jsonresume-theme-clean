f0322f4cc0e77112ee19fc6094ee20496cdff0b6
Tudor Jianu <tudorjnu@gmail.com>
2024-10-08 15:55:28 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const pug = require('pug')
-const helper = require('./assets/helper')
-const path = require('path')
+const pug = require("pug");
+const helper = require("./assets/helper");
+const path = require("path");
 
-const render = (resume) => pug.renderFile(path.join(__dirname, 'assets', 'template.pug'), {
-  resume,
-  helper
-})
+const render = (resume) =>
+  pug.renderFile(path.join(__dirname, "assets", "template.pug"), {
+    resume,
+    helper,
+  });
 
 const pdfRenderOptions = {
-  mediaType : 'print'
-}
+  mediaType: "print",
+};
 
 module.exports = {
   render,
-  pdfRenderOptions
-}
+  pdfRenderOptions,
+};
